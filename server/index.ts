1598e19f8896be04021379820403eea78ba27865
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-11-16 20:22:37 +0530

    cors

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
